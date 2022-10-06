3bb0dadd44605f2645bb0ea8e5fe53cd9da2e309
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-10-06 23:47:04 +0200

    Initial commit

---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestApplication {
+    @Test
+    public void NormalTest(){
+        assertEquals((2+3),5);
+    }
+}
