9cafc39ca37ab445bd32995a17e6752f8798e6bc
vicky-dhbw <vic.sharma.21@lehre.mosbach.dhbw.de>
2022-10-28 15:56:13 +0200

    doing..

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,9 @@
+import spaceShips.SpaceShipTwo;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Will finish this project");
+
+        SpaceShipTwo spaceShipTwo=new SpaceShipTwo();
     }
 }
