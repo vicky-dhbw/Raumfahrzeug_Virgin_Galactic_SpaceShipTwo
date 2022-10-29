d33b5e0be5031bb15a9f13b8a6abfb98a50a1edf
vicky-dhbw <vic.sharma.21@lehre.mosbach.dhbw.de>
2022-10-29 23:48:27 +0200

    done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,9 +4,19 @@ import spaceShips.WhiteKnightTwo;
 public class Application {
     public static void main(String[] args) {
 
+
+        // this application does not contain implementation of SpacePort / tank vehicle / Tower assigned to S05
+        // Reason: S05 has been ex-matriculated
+        // -------------------------------------------------------------------------------------------------------
+        // this application therefore only contains implementation of tasks assigned to S06
+        // following has been implemented:
+        // --> modelling of VSS and Mother ship  (all that is relevant for spaceship and mother ship
+        // --> abstraction related to journey of the VSS and Mother ship  (all abstractions related to journey to the space)
+        // --> wishing you a good journey to space :)
+
+
         SpaceShipTwo spaceShipTwo=new SpaceShipTwo();
         WhiteKnightTwo whiteKnightTwo=new WhiteKnightTwo();
-
 
         whiteKnightTwo.setSpaceShipTwo(spaceShipTwo);
         spaceShipTwo.setWhiteKnightTwo(whiteKnightTwo);
